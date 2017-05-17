a220f790e60f65bb4db7f7766653869f7dfa2846
IMS-Global <brennick.sci@gmail.com>
2017-05-17 17:16:26 -0600

    Added Documentation

--- calc.rb
+++ calc.rb
@@ -1,6 +1,8 @@
 # @author Brennick Langston
 # @version 1.0.0
 
+# Displays the Main Menu
+# @return
 def display_menu
   puts "\n\nMain Menu"
   puts "1) Enter Variables"
@@ -9,12 +11,17 @@ def display_menu
   select_option
 end
 
+# Obtains an option selected from the user
+# @return nil
 def select_option
   puts "Enter Option:\s"
   option = gets.strip
   process_option(option)
 end
 
+# Determines which subapplication should be run
+# @param option <String> user selected input option
+# @return nil
 def process_option(option)
   case option.to_i
   when 1
@@ -28,16 +35,22 @@ def process_option(option)
   end
 end
 
+# Exits the application
+# @return nil
 def exit_application
   puts "\n\nExiting application!"
   exit
 end
 
+# Displays the error menu for invalid user input variables
+# @return nil
 def show_error_menu
   puts "\n\nInvalid Input! Please try again."
   display_menu
 end
 
+# Displays the menu for getting user variables as input
+# @return nil
 def show_variable_menu
   print "First Variable:\s"
   var1 = gets.strip
@@ -49,16 +62,30 @@ def show_variable_menu
   calculate_vars(var1, op, var2)
 end
 
+# Checks the validity of the input variables
+# @param var1 <String> left hand equation variable
+# @param op <String> operator for calculations
+# @param var2 <String> Right hand equation variable
+# @return nil
 def check_var_inputs(var1, op, var2)
   check_num_inputs(var1, var2)
   show_error_menu unless op !~ /[0-9a-zA-Z]+/
 end
 
+# Check the valididty of the user numeric input Variables
+# @param var1 <String> left hand variable
+# @param var2 <String> Right hand variable
+# @return nil
 def check_num_inputs(var1, var2)
   show_error_menu if var1 =~ /[a-zA-Z]+/
   show_error_menu if var2 =~ /[a-zA-Z]+/
 end
 
+# Does calculations using the user input variables that were submitted
+# @param var1 <String> left hand equation variable
+# @param op <String> operator for calculations
+# @param var2 <String> Right hand equation variable
+# @return nil
 def calculate_vars(var1, op, var2)
   ans = var1.to_f.send(op,var2.to_f)
   if !ans.nil?
@@ -69,6 +96,8 @@ def calculate_vars(var1, op, var2)
   display_menu
 end
 
+# Displays the menu for acquiring a string equation
+# @return nil
 def show_eq_menu
   puts "Enter an equation like [5-7+3+(10*10)+(20+20)]:\s"
   eq = gets.strip
@@ -77,6 +106,9 @@ def show_eq_menu
   display_menu
 end
 
+# Parser for equations submitted as strings
+# @param eq <String> simple equation
+# @return <String> reduced answer
 def parse_eq_string(eq)
   eq_regex = /\((\d+)([\+|\-|\*|\/])(\d+)\)/
   # parenthesis (P)
@@ -101,8 +133,3 @@ end
 
 # start the application
 display_menu
-
-
-#str = '5-7+3+(10*10)+(20+20)'
-#puts str
-#parse_eq_string(str)
